25ffc1b4ac722e135bb6f1f3cbf62061a98cb4f0
Lingzh022 <reallingzh@163.com>
2021-03-30 08:41:37 +0800

    完成了登录功能

--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// message 导入弹框提示组件 需要全局挂载
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+Vue.prototype.$message = Message
